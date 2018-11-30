b89ef64ed2366797ee3e4f0ad8c2c853a5c159fd
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-11-30 01:31:13 -0600

    Copy sources; fix

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const compiler = require('./lib/compiler');
 
 const destDir = `${__dirname}/e2e`;
 const srcDir = `${__dirname}/tests`;
+const copy = [`${__dirname}/tests/helpers`];
 const stepFiles = [`${__dirname}/tests/steps/searching.js`];
 
 compiler({
+  copy,
   srcDir,
   destDir,
   stepFiles,
