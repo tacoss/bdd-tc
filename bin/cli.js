b07c88e2f0b8e7cad0b7d648a7cf47cb24aecb3c
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-12-06 14:54:32 -0600

    Revert scope fix; add compiler CLI

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,42 @@
-console.log(42);
+const wargs = require('wargs');
+
+const pwd = process.cwd();
+
+const argv = wargs(process.argv.slice(2), {
+  default: {
+    src: `${pwd}/tests/e2e`,
+    dest: `${pwd}/generated`,
+  },
+  alias: {
+    s: 'src',
+    d: 'dest',
+    c: 'copy',
+    t: 'steps',
+  },
+});
+
+function toArray(value) {
+  if (!value) {
+    return [];
+  }
+
+  return !Array.isArray(value)
+    ? [value]
+    : value;
+}
+
+try {
+  const compiler = require('../lib/compiler');
+
+  compiler({
+    srcDir: argv.flags.src,
+    destDir: argv.flags.dest,
+    copyFrom: toArray(argv.flags.copy),
+    stepFiles: toArray(argv.flags.steps),
+  });
+
+  process.stdout.write(argv.flags.dest);
+} catch (e) {
+  process.stderr.write(e.message);
+  process.exit(1);
+}
--- tests/e2e/steps/searching.js
+++ tests/e2e/steps/searching.js
@@ -10,19 +10,19 @@ export default {
   'Given open "$searchEngine" URL' (searchEngine) {
     page = pages[searchEngine];
 
-    await this
+    await t
       .navigateTo(page.url)
       .expect(page.body.visible).ok();
   },
 
   'When I $find for "$searchQuery"' (searchQuery) {
-    await this
+    await t
       .typeText(page.search.input, searchQuery)
       .click(page.search.submit);
   },
 
   'Then should I see "$searchResult"' (searchResult) {
-    await this
+    await t
       .expect(page.search.output.innerText).contains(searchResult);
   },
 };
