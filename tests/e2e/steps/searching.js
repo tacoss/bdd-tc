ccd0182e449c286ec97629763449a404c71b517b
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-12-06 14:24:15 -0600

    Inject t as context; fix

--- tests/e2e/steps/searching.js
+++ tests/e2e/steps/searching.js
@@ -10,19 +10,19 @@ export default {
   'Given open "$searchEngine" URL' (searchEngine) {
     page = pages[searchEngine];
 
-    await t
+    await this
       .navigateTo(page.url)
       .expect(page.body.visible).ok();
   },
 
   'When I $find for "$searchQuery"' (searchQuery) {
-    await t
+    await this
       .typeText(page.search.input, searchQuery)
       .click(page.search.submit);
   },
 
   'Then should I see "$searchResult"' (searchResult) {
-    await t
+    await this
       .expect(page.search.output.innerText).contains(searchResult);
   },
 };
